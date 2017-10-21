ac1483db0249c8ff578974c25e31900035fe543c
Dennis van Dalen <denemio@gmail.com>
2017-10-21 11:05:56 +0200

    no message

--- sample/src/main/java/uk/co/samuelwall/materialtaptargetprompt/sample/MainActivity.java
+++ sample/src/main/java/uk/co/samuelwall/materialtaptargetprompt/sample/MainActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Dennis van Dalen
+ * Copyright (C) 2016 Samuel Wall
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
